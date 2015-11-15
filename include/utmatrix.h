33a7bdac60c15cac571842aa2d0bf9b331819557
IlyaKiparenko <ilya.kiparenko@gmail.com>
2015-11-15 04:13:13 +0400

    Update utmatrix.h

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -250,10 +250,9 @@ TMatrix<ValType>& TMatrix<ValType>::operator=(const TMatrix<ValType> &mt) {
   if (this != &mt) {
     Size = mt.Size;
     StartIndex = mt.StartIndex;
-    delete [] pVector;
     pVector = new TVector<ValType>[Size];
     for (int i = 0; i < Size; i++) {
-      pVector[i] = mt.pVector[i]; // *(new TVector<ValType>(mt.pVector[i]));
+      pVector[i] = *(new TVector<ValType>(mt.pVector[i]));
     }
     return *this;
   }
